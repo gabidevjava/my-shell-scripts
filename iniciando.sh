28698c71dbe941f1167915028284bda8ed5a171b
Gabriela Guedes <zooiv3rde@gmail.com>
2019-08-12 21:58:34 -0300

    finalizando esse sh de inicio

--- iniciando.sh
+++ iniciando.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-
+echo -e "\033[31mSeus arquivos\033[0m"
 ls
+echo
 echo -e "\033[31mCaminho da sua pasta atual\033[0m"
 pwd
 echo
+echo -e "\033[31mVersão do seu kernel\033[0m"
 uname -a
 echo
+echo -e "\033[033mUsuários do sistema\033[0m"
 users
+echo
 echo -e "\033[31mScript finalizado\033[0m"
