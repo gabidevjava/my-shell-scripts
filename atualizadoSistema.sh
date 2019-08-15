5115d891915d40d317fa2b2d6211c6e8f886b89b
Gabriela Guedes <zooiv3rde@gmail.com>
2019-08-14 21:16:02 -0300

    script para atualizar meu sistema

---
+++ atualizadoSistema.sh
@@ -0,0 +1,9 @@
+echo -e "\033[31mIniciando atualização do pacman todo Poderoso\033[0m"
+echo
+sudo pacman -Syyu --noconfirm
+echo
+echo -e "\033[31mIniciando atualização do yaourt god dos god\033[0m"
+yaourt -Syyua --noconfirm
+echo
+echo -e "\033[44mGaby, sistema atualizado!\033[0m"
+
