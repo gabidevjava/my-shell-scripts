4f4742d078a0cd5746b5b02aec2b5a58874cce55
Gabriela <zooiv3rde@gmail.com>
2020-02-26 21:33:52 -0300

    att script para atualizacao do sistema manjaro com pamac

--- atualizandoSistema.sh
+++ atualizandoSistema.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
+echo -e "Atualizando mirrors"
+echo
+sudo pacman-mirrors -g
+echo
+
 echo -e "\033[31mIniciando atualização do pacman todo Poderoso\033[0m"
 echo
 sudo pacman -Syyu --noconfirm
 echo
 echo -e "\033[31mIniciando atualização do yaourt god dos god\033[0m"
-yaourt -Syyua --noconfirm
+pamac upgrade --aur --no-confirm
 echo
 echo -e "\033[44mGaby, sistema atualizado!\033[0m"
 
